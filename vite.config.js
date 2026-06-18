e96870a3fae2bfaedacb6ae53d13384b2993f0a9
Ashuthecoder <ashutoshkumarshah8512@gmail.com>
2026-06-18 16:32:39 +0530

    All done!!!

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
- base: '/JobTracker/',
+ base: '/JobTracker',
  plugins: [tailwindcss(), react(), babel({ presets: [reactCompilerPreset()] })],
 });
