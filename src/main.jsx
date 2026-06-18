98c7c1fefa444ad81b32872d21e2a72757018282
Ashuthecoder <ashutoshkumarshah8512@gmail.com>
2026-06-18 16:23:48 +0530

    All done!!

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+/** @format */
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
 // import App from "./App.jsx";
 import { HashRouter } from 'react-router-dom';
-import MainRoutes from "./routes/MainRoutes";
-import { JobContextProvider } from "./context/jobContextProvider";
+import MainRoutes from './routes/MainRoutes';
+import { JobContextProvider } from './context/jobContextProvider';
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <JobContextProvider>
-      <HashRouter>
-        <MainRoutes />
-      </HashRouter>
-    </JobContextProvider>
-  </StrictMode>,
+createRoot(document.getElementById('root')).render(
+ <StrictMode>
+  <JobContextProvider>
+   <HashRouter>
+    <MainRoutes />
+   </HashRouter>
+  </JobContextProvider>
+ </StrictMode>,
 );
